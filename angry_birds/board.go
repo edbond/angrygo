fa992144a6aa1c36d0f60bf6d619c024c27595fc
Eduard Bondarenko <edbond@gmail.com>
2017-06-10 23:34:23 +0300

    add figures on board type

--- angry_birds/board.go
+++ angry_birds/board.go
@@ -2,28 +2,62 @@ package angry_birds
 
 import (
 	"fmt"
-	"unicode/utf8"
+	"strings"
 )
 
 type Pig rune
 
-type Board [][]Pig
+type Figure []string
+
+// FigureOnBoard Figure positioned on board
+// x - horizontal left to right
+// y - vertical top down
+type FigureOnBoard struct {
+	figure Figure
+	x      int
+	y      int
+}
+
+type Board struct {
+	board   [][]Pig
+	figures []FigureOnBoard
+}
+
+// String function prints Board with figures
+func (b Board) String() string {
+	output := ""
+
+	// Print background
+	for _, row := range b.board {
+		rs := make([]string, 0)
+		for _, x := range row {
+			rs = append(rs, string(x))
+		}
+		s := strings.Join(rs, "")
+		output += s + "\n"
+	}
+
+	// TODO: Print figures
+
+	return output
+}
 
 func pigs(s string) []Pig {
-	var p = make([]Pig, utf8.RuneCountInString(s))
-	for r := range []rune(s) {
+	var p = make([]Pig, 0)
+	for _, r := range []rune(s) {
 		p = append(p, Pig(r))
 	}
 	return p
 }
 
-var board = Board{
-	pigs("ABC"),
-	pigs("BCA"),
-	pigs("ABA"),
+var exampleBoard = Board{
+	[][]Pig{
+		pigs("ABC"),
+		pigs("BCA"),
+		pigs("ABA"),
+	},
+	[]FigureOnBoard{},
 }
-
-type Figure []string
 
 func (figure Figure) String() string {
 	output := "\n"
@@ -38,15 +72,11 @@ func (figure Figure) RotateCW() Figure {
 	m := len(figure)
 	n := len(figure[0])
 
-	//fmt.Println("Figure\n", figure)
-
 	rows := []string{}
 	for i := 0; i < n; i++ {
 		var row string
 		for j := 0; j < m; j++ {
 			c := string(figure[m-j-1][i])
-			//fmt.Println("Char=", c)
-			//fmt.Println("i=", i, "j=", j, "row=", row)
 			row += string(c)
 		}
 		rows = append(rows, row)
@@ -71,4 +101,22 @@ func (figure Figure) equal(other Figure) bool {
 
 func (figure Figure) notEqual(other Figure) bool {
 	return !figure.equal(other)
+}
+
+// positions returns slice of possible positions of figure on board
+func Positions(figure Figure, board Board) []FigureOnBoard {
+	p := make([]FigureOnBoard, 0)
+	bw, bh := len(board.board[0]), len(board.board)
+	fw, fh := len(figure[0]), len(figure)
+
+	fmt.Println("Board ", bw, " x ", bh)
+	fmt.Println("Figure ", fw, " x ", fh)
+
+	for x := 0; x <= bw-fw; x++ {
+		for y := 0; y <= bh-fh; y++ {
+			p = append(p, FigureOnBoard{figure, x, y})
+		}
+	}
+
+	return p
 }
--- angry_birds/board_test.go
+++ angry_birds/board_test.go
@@ -1,6 +1,9 @@
 package angry_birds
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestNotEqual(t *testing.T) {
 	f1 := Figure{"X"}
@@ -30,4 +33,54 @@ func TestFigureRotation(t *testing.T) {
 	if f1Rotated.notEqual(rotated) {
 		t.Error("Rotated != expected,", f1Rotated, rotated)
 	}
+}
+
+func TestPositions(t *testing.T) {
+	f := Figure{
+		".X.",
+		"XXX",
+	}
+
+	b := Board{
+		[][]Pig{
+			pigs("ABC"),
+			pigs("BCD"),
+			pigs("ABC"),
+		},
+		[]FigureOnBoard{},
+	}
+
+	fmt.Println(b)
+
+	p := Positions(f, b)
+	if len(p) != 2 {
+		fmt.Println(p)
+		t.Error("Positions != 2", len(p))
+	}
+}
+
+func TestBoardPrinting(t *testing.T) {
+	f := Figure{
+		"..X",
+		"XXX",
+	}
+
+	b := Board{
+		[][]Pig{
+			pigs("ABCD"),
+			pigs("BCDA"),
+			pigs("DACB"),
+		},
+		[]FigureOnBoard{
+			{f, 1, 1},
+		},
+	}
+
+	result := fmt.Sprintf("%v", b)
+	expected := "\nABCD\nBCDX\nDXXX"
+
+	if result != expected {
+		t.Errorf("%s\n%s\n%s\n%s", "Board printed incorrectly", result, "Expected:", expected)
+	}
+
 }
